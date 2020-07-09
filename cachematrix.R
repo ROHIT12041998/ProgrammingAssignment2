d54c89f1654a3662d88cc9ea605c8b011266d5c7
ROHIT <rohitkumar1241998@gmail.com>
2020-07-09 21:46:21 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,38 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## The following function calculates the mean of the special "vector" created with the above function.
-## However, it first checks to see if the mean has already been calculated. If so, it gets the mean from 
-## the cache and skips the computation. Otherwise, it calculates the mean of the data 
+
+## The  function calculates the mean of the special "vector" created.
+## it first checks to see if the mean has already been calculated. If so, it gets the mean from 
+## the storage and skips the computation.or else, it calculates the mean of the data 
 ## and sets the value of the mean in the cache via the setmean function.
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
+  inver <- NULL
   set <- function(y){
     x <<- y
-    inv <<- NULL
+    inver <<- NULL
   }
   get <- function() x
-  setInverse <- function(solveMatrix) inv <<- solveMatrix
-  getInverse <- function() inv
+  setInverse <- function(solveMatrix) inver <<- solveMatrix
+  getInverse <- function() inver
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## cacheSolve: This function computes the inverse of the special "matrix" 
-## returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed),
-## then the cachesolve should retrieve the inverse from the cache. Computing the inverse of a square matrix can be
-## done with the solve function in R. For example,
-## if X is a square invertible matrix, then solve(X) returns its inverse.
+##  computes the inverse of the special "matrix" returned by makeCacheMatrix.
+##  If the inverse is already calculated (and the matrix does not change),
+## then the cachesolve should retrieve the inverse from the cache. Computes the inverse of a square matrix 
 
+## which can be done with the solve function in R.
 cacheSolve <- function(x, ...) {
-  inv <- x$getInverse()
-  if(!is.null(inv)){
+  inver <- x$getInverse()
+  if(!is.null(inver)){
     message("getting cached data")
-    return(inv)
+    return(inver)
   }
   data <- x$get()
-  inv <- solve(data)
-  x$setInverse(inv)
-  inv      
+  inver <- solve(data)
+  x$setInverse(inver)
+  inver      
 }
